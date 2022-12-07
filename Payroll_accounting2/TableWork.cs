bef2831a8316b8e761901fcdad566b4597a67d4d
Ilyas Gimranov <ilyas.gimranov.2003@mail.ru>
2022-12-07 22:56:28 +0300

    Добавьте файлы проекта.

---
+++ Payroll_accounting2/TableWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Payroll_accounting2
+{
+    public partial class TableWork : Form
+    {
+        public TableWork()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonMainMenu_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show();
+        }
+    }
+}
